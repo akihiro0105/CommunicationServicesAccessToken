d97e122bdc56df02e0f2b236d6867dd8e52f94e2
akihiro <ueyama0105@gmail.com>
2021-06-11 08:53:49 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using System;
+using Azure;
+using Azure.Core;
+using Azure.Communication.Identity;
+
+namespace AccessTokensQuickstart
+{
+    class Program
+    {
+        private static readonly string endpoint = "<Endpoint>";
+        private static readonly string accessKey = "<AccessKey>";
+        // 作成したユーザーを削除する場合は以下を true
+        private static readonly bool isDeleteUser = false;
+
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            // 認証
+            var client = new CommunicationIdentityClient(new Uri(endpoint), new AzureKeyCredential(accessKey));
+
+            // ID作成とToken発行
+            var identityAndTokenResponse = await client.CreateUserAndTokenAsync(scopes: new[] { CommunicationTokenScope.VoIP });
+            var user = identityAndTokenResponse.Value.User;
+            var token = identityAndTokenResponse.Value.AccessToken.Token;
+
+            Console.WriteLine(token);
+
+            // ID削除
+            if (isDeleteUser)
+            {
+                await client.DeleteUserAsync(user);
+                Console.WriteLine($"\nDelete ID: {user.Id}");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # CommunicationServicesAccessToken
+- Azure Communication Services 利用時にサーバー側で行うユーザー作成とアクセストークンの発行行うサンプル
+
+1. Visual Studio Code で CommunicationServicesAccessToken フォルダを開く
+2. `統合ターミナルで開く`を選択してpowershellを起動
+    ![alt](./doc/01.png)
+3. 以下のコマンドを実行して必要なパッケージをインストール
+    - `dotnet add package Azure.Communication.Identity --version 1.0.0`
+4. `Program.cs` の `endpoint` と `accessKey` に Azure Communication Services から取得した値を入力
+5. ビルドを行い
+    - `dotnet build`
+6. 実行するとアクセストークンが発行される
+    - `dotnet run`
+
+*アクセストークンをファイルに保存しておきたい場合には `dotnet run > token.txt` でファイル出力*
